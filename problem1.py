3f77dda9c75215ab51a301254a08ace68d616de2
Connor9998 <connorm298@deltalearns.ca>
2024-12-10 11:32:32 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -28,7 +28,29 @@ The roots are -4.0 and -4.0
 """
 import os
 os.system('cls')
-
+import math
+import cmath
 
 print("Enter in the coefficients for a quadratic equation in the format:")
 print("  ax^2 + bx + c = 0")
+#convert to a number
+a=input("Emter a number: ")
+#convert to a number
+a=float(a)
+b=input("Enter a number: ")
+#convert to a number
+b=float(b)
+c=input("Enter a number: ")
+#convert to a number
+c=float(c)
+
+x1 = (math.sqrt((pow(b,2)-4*a*c))-b) / (2*a)
+x1=float(x1)
+x2 = (- 1*math.sqrt((pow(b,2)-4*a*c))-b)/(2*a)
+x2=float(x2)
+try:
+  if x1== isnumeric(x1) and x2 == isnumeric(x2):  
+    print(x1,x2)
+
+except:
+  print("Those are not valid values for a, b or c")
